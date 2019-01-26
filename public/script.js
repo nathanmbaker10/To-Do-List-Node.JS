99418d5851a7e04fc5facc2096e25f3ac5b44b44
Nathan <nathanmbaker@gmail.com>
2019-01-25 16:02:42 -0800

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,65 @@
+function newItemClicked() {
+  var taskNameBox = document.getElementById("taskNameBox")
+  var dueDateBox = document.getElementById("dueDateBox")
+  taskNameBox.placeholder = "Task Name"
+  dueDateBox.placeholder = "Due Date"
+  console.log("Hello")
+  var taskObject = {
+    name: document.getElementById("taskNameBox").value,
+    dueDate: document.getElementById("dueDateBox").value
+  };
+  taskNameBox.value = ""
+  dueDateBox.value = ""
+  taskLength++;
+  console.log("Task Length: " + taskLength);
+  $.ajax ({
+        url: 'http://localhost:3000/api/additem?name=' + taskObject.name + '&dueDate=' + taskObject.dueDate,
+        type: "POST",
+        success: populateTable
+})
+}
+
+function populateTable(taskData) {
+  console.log("Populate!");
+  document.getElementById("taskTable").innerHTML = ""
+  var taskTable = document.getElementById("taskTable")
+  taskLength = taskData.length
+  console.log("Server task length:" + taskLength);
+  for (var i = 0; i < taskData.length; i++) {
+    console.log()
+    addRow(taskData[i].name, taskData[i].dueDate, taskData[i]["_id"])
+  }
+  addButtonOnClick()
+}
+
+$(document).ready(function(){
+    addButtonOnClick()
+  })
+
+function addRow(name, dueDate, id){
+  var newRow = document.createElement("tr");
+  var itemCell = document.createElement("td");
+  var dueDateCell = document.createElement("td");
+  var removeCell = document.createElement("td");
+  var removeButton = document.createElement("button")
+  removeButton.id = id
+  removeButton.classList.add("removeButton")
+  removeButton.innerHTML = "X"
+  itemCell.innerHTML = name
+  dueDateCell.innerHTML = dueDate
+  removeCell.appendChild(removeButton)
+  newRow.appendChild(itemCell);
+  newRow.appendChild(dueDateCell)
+  newRow.appendChild(removeCell)
+  document.getElementById("taskTable").appendChild(newRow)
+}
+
+function addButtonOnClick(){
+  $(".removeButton").on('click', function(){
+    $.ajax ({
+        url: 'http://localhost:3000/api/buttonclick?id=' + this.id,
+        type: "POST",
+        success: populateTable
+       })
+    })
+}
---
+++ server.js
@@ -0,0 +1,77 @@
+// call the packages we need
+var express    = require('express')      // call express
+var bodyParser = require('body-parser')
+var app        = express()     // define our app using express
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+
+// configure app to use bodyParser() and ejs
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use( express.static(__dirname + "/public"))
+app.set('view engine','ejs');
+
+// get an instance of the express Router
+var router = express.Router();
+
+// a “get” at the root of our web app: http://localhost:3000/api
+router.get('/', function(req, res) {
+  console.log("GET")
+  var dataResult
+
+  db.collection('tasks').find({}).toArray((err, result) => {
+    if(err) {console.log(err)}
+    res.render('index.ejs', {resultData: result})
+
+  })
+
+
+});
+
+router.post('/buttonclick', function(req, res) {
+  console.log("BUTTON")
+  var id = req.query.id
+  console.log(id);
+  db.collection('tasks').deleteOne({"_id": ObjectId(id)}, function(err, res_) {
+    if (err) console.log(err);
+    db.collection('tasks').find({}).toArray((err, result) => {
+      res.send(result)
+
+    })
+  })
+
+})
+
+router.post('/additem', function(req, res) {
+  console.log("NEW ITEM")
+  var name = req.query.name
+  var dueDate = req.query.dueDate
+  var tag = req.query.tag
+   console.log("post")
+  db.collection('tasks').insertOne({name: name, dueDate: dueDate}, function(){
+    console.log("REDIRECT")
+    db.collection('tasks').find({}).toArray((err, result) => {
+      if(err) {console.log(err)}
+      console.log("About to render")
+      res.send(result)
+
+    })
+  })
+})
+// all of our routes will be prefixed with /api
+
+
+// START THE SERVER
+//==========================================================
+var db
+MongoClient.connect('mongodb://nathanmbaker:Soccerguy1@ds055689.mlab.com:55689/to-do-list-nathan',{ useNewUrlParser: true }, (err, client) => {
+    if(err) console.log(err)
+    console.log("Connected successfully to server");
+
+    db = client.db('to-do-list-nathan');
+    app.listen(3000, () => {
+        console.log('WORKING')
+    })
+})
+
+app.use('/api', router);
